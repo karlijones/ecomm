0614b5e79c088dd9ee7c5276e4b5f795d4b6294b
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-14 10:46:54 -0500

    route handler callback function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hi there!');
+const express = require('express');
+
+//app is an object that describes all the different things that our web server can do
+const app = express();
+//route handler
+app.get('/', (req, res) => {
+    res.send('hi there!');
+});
+
