a145c5c7bc52267d85e9a037ccb842cd3d71df84
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-17 13:00:15 -0500

    try/catch for filename

--- repositories/users.js
+++ repositories/users.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+
 class UsersRepository {
     constructor(filename) {
         if (!filename) {
             throw new Error('Creating a repository requires a filename');
         }
+
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+        
+    }
+
+    checkForFile() {
+
     }
 }
 
