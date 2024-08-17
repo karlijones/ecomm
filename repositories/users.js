56af533a28b0268d6839102c945173b6e55978c3
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-17 16:49:12 -0500

    updating records

--- repositories/users.js
+++ repositories/users.js
@@ -53,12 +53,24 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('86a59275');
+    await repo.update('d3d92dc4', { password: 'mypassword' });
 
     console.log(user);
 };
