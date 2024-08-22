c255c371672b9d7a01aab5c8477071820f8d2abb
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-22 18:44:47 -0500

    signup layout file

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,15 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-    return `
-   
-    <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-            <input placeholder="email" />
-            <input placeholder="password" />
-            <input placeholder="password confirmation" />
-            <button>Sign Up</button>
-        </form>
-    </div>
-        
+    return layout({ 
+        content: `
+        <div>
+            Your id is: ${req.session.userId}
+            <form method="POST">
+                <input placeholder="email" />
+                <input placeholder="password" />
+                <input placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
     `
+    });
 };
