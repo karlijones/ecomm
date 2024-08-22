58b10acf71eb51a4058f9ac657c18a284d431dbb
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-22 18:59:24 -0500

    express validator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { check } = require('express-validator');
+//Destructuring here because we only want the check feature from express validatior
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
