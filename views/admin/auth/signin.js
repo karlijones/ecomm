1e0fd2326d68212fb08e9b6e79d8ec72a065470b
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-22 18:39:42 -0500

    layout for signin file

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,14 +1,17 @@
-module.exports = () => {
-    return ` 
-     
-    <div>
-        <form method="POST">
-            <input name="email" placeholder="email" />
-            <input name="password" placeholder="password" />
-            <button>Sign In</button>
-        </form>
-    </div>
-       
-        `;
+const layout = require('../layout');
 
+
+module.exports = () => {
+    return layout({ 
+        content: ` 
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+       
+        `
+    });
 };
