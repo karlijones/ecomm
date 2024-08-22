4111ec7435366ddc8375152c34bca48b59a08753
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-22 18:35:05 -0500

    setup for html layout file

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,15 @@
 module.exports = ({ req }) => {
     return `
+   
     <div>
-            Your id is: ${req.session.userId}
-            <form method="POST">
-                <input placeholder="email" />
-                <input placeholder="password" />
-                <input placeholder="password confirmation" />
-                <button>Sign Up</button>
-            </form>
-        </div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+            <input placeholder="email" />
+            <input placeholder="password" />
+            <input placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+        
     `
 };
