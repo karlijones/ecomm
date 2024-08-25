e8aca7f5b35f1d0b5acdb44fb028cefbd952df6c
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-25 13:44:56 -0500

    Refactoring reqirePasswordConfirmation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword } = require('./validators');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
@@ -15,19 +15,7 @@ router.get('/signup', (req, res) => {
 
 router.post(
     '/signup', 
-    [   
-    requireEmail,
-    requirePassword,
-    check('passwordConfirmation')
-    .trim()
-    .isLength({ min:4, max:20 })
-    .withMessage('Must be between 4 and 20 characters')
-    .custom((passwordConfirmation, { req }) => {
-        if (passwordConfirmation !== req.body.password) {
-            throw new Error('Passwords must match');
-        }
-    })
-    ], 
+    [ requireEmail, requirePassword, requirePasswordConfirmation], 
 async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
