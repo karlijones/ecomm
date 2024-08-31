9cbe8f914472a9acdea691cbee82c60e64842b44
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-30 22:20:38 -0500

    product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = reqire('./routes/admin/routes/products');
 
 //app is an object that describes all the different things that our web server can do
 const app = express();
@@ -12,6 +13,7 @@ app.use(cookieSession({
     keys: ['woaspoidjkly']
 }));
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
     console.log('Listening');
