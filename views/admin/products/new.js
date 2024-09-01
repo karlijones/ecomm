6753da78f1d060701933af274cfe27b4ca768981
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-31 20:07:58 -0500

    html added for new products file

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,7 +4,12 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
     return layout({
         content: `
-        
+            <form method="POST">
+                <input placeholder="Title" name="title" />
+                <input placeholder="Price" name="price" />
+                <input type="file" name="image" />
+                <button>Submit</button>
+            </form>
         `
     });
 };
