dc87e85f38e27617714a66b8f54c19e7b72016c4
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-01 13:32:15 -0500

    accessing image upload file

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -15,13 +15,10 @@ router.get('/admin/products/new', (req, res) => {
     res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], upload, (req, res) => {
+router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'), (req, res) => {
     const errors = validationResult(req);
     
-    req.on('data', data => {
-        console.log(data.toString());
-    });
-    
+    console.log(req.file);
     
     res.send('submitted');
 });
