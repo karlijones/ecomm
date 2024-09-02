cdce6693c7a8c113307a24d95bf5130a6628a687
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-02 12:00:44 -0500

    updating require statements

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
