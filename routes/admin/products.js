8a72446c426886899c9e21be800df07e1b2eb85a
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-04 20:26:01 -0500

    middleware to handle requiring authentication

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -11,17 +11,26 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage()});
 
 router.get('/admin/products', async (req, res) => {
+    if (!req.session.userId) {
+        return res.redirect('/signi');
+    }
     const products = await productsRepo.getAll();
     res.send(productsIndexTemplate({ products }));
 });
 
 router.get('/admin/products/new', (req, res) => {
+    if (!req.session.userId) {
+        return res.redirect('/signi');
+    }
     res.send(productsNewTemplate({}));
 });
 
 router.post('/admin/products/new', upload.single('image'), [requireTitle, requirePrice],  
 handleErrors(productsNewTemplate),
 async (req, res) => {
+    if (!req.session.userId) {
+        return res.redirect('/signi');
+    }
     const image = (req.file.buffer.toString('base64'));
     const { title, price } = req.body;
     await productsRepo.create({ title, price, image });
