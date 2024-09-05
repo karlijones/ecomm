ca31aca77f76707a856e06110a0e11b1a8ca7e54
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-04 20:06:47 -0500

    redirect in auth for signin and signup

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -22,7 +22,7 @@ router.post(
 
         req.session.userId = user.id;
 
-        res.send('Account created!!!');
+        res.redirect('/admin/products');
 });
 
 router.get('/signout', (req, res) => {
@@ -44,7 +44,7 @@ router.post('/signin',
         
         req.session.userId = user.id;
 
-        res.send('You are signed in!!!');
+        res.redirect('/admin/products');
     }
 );
 
