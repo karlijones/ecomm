8c72e071e404270e0945b4a0336df4791f0b2b3d
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 14:52:59 -0500

    route handler for adding item to cart

--- routes/carts.js
+++ routes/carts.js
@@ -3,6 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 //Receive a post request to add an item to a cart
+router.post('/cart/products', (req, res) => {
+    console.log(req.body.productId);
+
+    res.send('Product added');
+});
 
 //Receive a GET request to show all items in cart
 
