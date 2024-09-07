25f2220befa15035bbaf7e926866eb3e2bcf598d
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 12:32:27 -0500

    additional user-facing products file

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const productsRepo = require('../repositories/products');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    const products = await productsRepo.getAll();
     res.send('Products!!!');
 });
 
