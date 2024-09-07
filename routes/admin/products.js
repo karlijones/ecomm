140ee5f466bbe5e8f16bae659cd80ce0892c2bdf
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 12:04:22 -0500

    route handler for deleting products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -64,4 +64,10 @@ router.post('/admin/products/:id/edit', requireAuth,
     }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
+
+    res.redirect('/admin/products');
+});
+
 module.exports = router;
