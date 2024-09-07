bc1f34250d1310897d619b3b5d1425836929b753
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 12:24:54 -0500

    distinguishing between router for user-facing products vs admin products

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 //app is an object that describes all the different things that our web server can do
 const app = express();
