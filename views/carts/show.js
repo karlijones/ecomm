6948e4adec33755a10afb0cf62591865425e618c
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-08 13:49:44 -0500

    additional code for displaying items in cart

--- views/carts/show.js
+++ views/carts/show.js
@@ -3,14 +3,14 @@ const layout = require('../layout');
 module.exports = ({ items }) => {
     const renderedItems = items.map((item) => {
         return `
-            <div>${item.product.title} - ${item.product.price}</div>
+            <div>${item.product.title} - ${item.product.price} - ${item.quantity}</div>
         `;
     })/join('');
 
     return layout({
        content: `
-       <h1>Cart</h1>
+       <h1>Cart Items</h1>
        ${renderedItems}
        ` 
-    })
+    });
 };
