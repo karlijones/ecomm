68e3b152f353030e34f9796cad009c8eefd66ea0
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-08 14:15:44 -0500

    route handler for deleting an item

--- routes/carts.js
+++ routes/carts.js
@@ -52,5 +52,8 @@ router.get('/cart', async (req, res) => {
 });
 
 //Receive a post request to delete an item from a cart
+router.post('/cart/products/delete', async (req, res) => {
+    console.log(req.body.itemId);
+});
 
 module.exports = router;
