01766c5f53c573ca0e6160e89e0edd16b6b86ebf
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 20:32:21 -0500

    refactor for scope

--- routes/carts.js
+++ routes/carts.js
@@ -6,14 +6,15 @@ const router = express.Router();
 //Receive a post request to add an item to a cart
 router.post('/cart/products', async (req, res) => {
     //Figure out the cart!
+    let cart;
     if (!req.session.cartId) {
         //We don't have a cart. We need to create one...
         //and store the cart id on the req.session.cartId property
-        const cart = await cartsRepo.create({ items: [] });
+        cart = await cartsRepo.create({ items: [] });
         req.session.cartId = cart.id;
     } else {
       // We have a cart! Let's get it from the respository. 
-      const cart = await cartsRepo.getOne(req.session.cartId); 
+      cart = await cartsRepo.getOne(req.session.cartId); 
     }
 
     //Either increment quantity for existing product
