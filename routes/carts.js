659eb59758d14d457c539e2d067ad5682afbd7f8
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-09-07 20:22:20 -0500

    if/else comments

--- routes/carts.js
+++ routes/carts.js
@@ -5,6 +5,12 @@ const router = express.Router();
 //Receive a post request to add an item to a cart
 router.post('/cart/products', (req, res) => {
     //Figure out the cart!
+    if (!req.session.cartId) {
+        //We don't have a cart. We need to create one...
+        //and store the cart id on the req.session.cartId property
+    } else {
+      // We have a cart! Let's get it from the respository.  
+    }
 
     //Either increment quantity for existing product
     //OR add new product to items array
